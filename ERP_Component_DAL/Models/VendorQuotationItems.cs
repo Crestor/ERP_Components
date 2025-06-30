179cce4586a79425eab5fcab76f79c08d9041d8a
screenprog <asimansari.mail@proton.me>
2025-06-30 12:03:37 +0530

    forgot to commit model; #12

---
+++ ERP_Component_DAL/Models/VendorQuotationItems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_Component_DAL.Models
+{
+    public class VendorQuotationItem
+    {
+        public VendorQuotationItem()
+        {
+            this.VendorQuotationItems = new List<VendorQuotationItem>();
+        }
+
+        public List<VendorQuotationItem>? VendorQuotationItems { get; set; }
+        public Guid ItemID { get; set; }
+        public string? ItemName { get; set; }
+        public string? UOM { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+    }
+}
