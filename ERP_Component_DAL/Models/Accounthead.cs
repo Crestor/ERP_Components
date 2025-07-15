430b5771f06a6d850c3ac53605b90b43f7ca461e
tushar44611 <129596154+tushar44611@users.noreply.github.com>
2025-07-15 14:48:54 +0530

    work on #111 and #106

---
+++ ERP_Component_DAL/Models/Accounthead.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_Component_DAL.Models
+{
+    public class Accounthead
+    {
+        public string AccountHeadName { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public string IFSC { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+    }
+}
